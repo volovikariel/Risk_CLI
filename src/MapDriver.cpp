a1d4280895022e3891704c441ef4ff5c1089402a
Ohmni <Ohmni>
2021-09-29 11:27:09 -0400

    Improving the driver interface

--- src/MapDriver.cpp
+++ src/MapDriver.cpp
@@ -2,43 +2,73 @@
 
 #include <iostream>
 
+bool sameString(const char* lhs, const char* rhs)
+{
+    return strcmp(lhs, rhs) == 0;
+}
+
+void analyzeFile(const char* filepath)
+{
+    Map map;
+    MapLoader loader;
+    auto result = loader.load(filepath, map);
+    std::cout << "Load error: " << result << std::endl << std::endl;
+
+    for (auto continent : map.getContinents())
+    {
+        std::cout << *continent << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    for (auto territory : map.getTerritories())
+    {
+        std::cout << *territory << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    for (auto territory : map.getTerritories())
+    {
+        std::cout << "Neighbors of: " << *territory << std::endl;
+
+        for (auto neighbor : territory->neighbors)
+        {
+            std::cout << "    " << *neighbor << std::endl;
+        }
+
+        std::cout << std::endl;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     // TODO load any number of maps
+    
+    bool displayHelp = argc > 1 && (
+        sameString(argv[1], "h") ||
+        sameString(argv[1], "help") ||
+        sameString(argv[1], "-h") ||
+        sameString(argv[1], "-help") ||
+        sameString(argv[1], "--h") ||
+        sameString(argv[1], "--help") ||
+        sameString(argv[1], "/h") ||
+        sameString(argv[1], "/help"));
 
-    if (argc > 1)
+    if (argc <= 1 || displayHelp)
+    {
+        std::cout << "Usage: provide one or more filepaths as arguments." << std::endl;
+        std::cout << "If only one filepath is provided: the file will be loaded and its load status, continent, and territory details will be displayed." << std::endl;
+        std::cout << "If multiple filepaths are provided: each file will be loaded and only its load status will be displayed." << std::endl;
+        std::cout << std::endl;
+    }
+    else if (argc == 2)
     {
         char* filepath = argv[1];
-
-        Map map;
-        MapLoader loader;
-        auto result = loader.load(filepath, map);
-        std::cout << "Load error: " << result << std::endl << std::endl;
-
-        for (auto continent : map.getContinents())
-        {
-            std::cout << *continent << std::endl;
-        }
-
-        std::cout << std::endl;
-
-        for (auto territory : map.getTerritories())
-        {
-            std::cout << *territory << std::endl;
-        }
-
-        std::cout << std::endl;
-
-        for (auto territory : map.getTerritories())
-        {
-            std::cout << "Neighbors of: " << *territory << std::endl;
-
-            for (auto neighbor : territory->neighbors)
-            {
-                std::cout << "    " << *neighbor << std::endl;
-            }
-
-            std::cout << std::endl;
-        }
+        analyzeFile(filepath);
+    }
+    else
+    {
+        // TODO multiple files
     }
 }
