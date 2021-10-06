713755a87b6de101b3314b47bd4d084631b406f9
volovikariel <16262535+volovikariel@users.noreply.github.com>
2021-10-06 12:03:30 -0400

    Non-final version, just there to start getting reviewed/used by others

--- src/Cards.h
+++ src/Cards.h
@@ -1,14 +1,75 @@
+#include "Orders.h"
+#include <stdlib.h>
+#include <vector>
+#include <assert.h>
+#include <ostream>
+using std::vector;
+
+class Hand;
+//TODO: Use references instead of pointers when passing in params (Ask ohmni)
 class Card
 {
-
+    public:
+        enum Card_Type {
+            bomb,
+            reinforcement,
+            blockade,
+            airlift,
+            diplomacy,
+            ENUM_SIZE
+        };
+        Card(enum Card_Type);
+        // Copy constructor
+        Card(const Card* other);
+        // Assignment operator
+        void operator = (const Card* other);
+        // Stream output operator
+        friend std::ostream& operator << (std::ostream& out, const Card* source);
+        ~Card();
+        Order* play();
+        Card_Type getType();
+        Hand* getHand();
+    private:
+        Card_Type type;
+        Hand* hand;
 };
 
 class Deck
 {
-
+    public:
+        Deck();
+        // Copy constructor
+        Deck(const Deck* other);
+        ~Deck();
+        // Assignment operator
+        void operator = (const Deck* other);
+        // Stream output operator
+        friend std::ostream& operator << (std::ostream& out, const Deck* source);
+        vector<Card*>* getCards();
+        void addCard(Card* card);
+        Card* drawCard();
+    private:
+        vector<Card*> cards;
 };
 
 class Hand
 {
-
+    public:
+        Hand();
+        // Copy constructor
+        Hand(const Hand* other);
+        // Assignment operator
+        void operator = (const Hand* other);
+        // Stream output operator
+        friend std::ostream& operator << (std::ostream& out, const Hand* source);
+        ~Hand();
+        void addCard(Card*);
+        void removeCard(int);
+        void removeCard(Card*);
+        Card* getCard(int);
+        vector<Card*>* getCards();
+    private:
+        vector<Card*> cards;
 };
+
+extern Deck main_deck;
--- src/CardsDriver.cpp
+++ src/CardsDriver.cpp
@@ -1,6 +1,32 @@
 #include "Cards.h"
+#include <time.h>
+#include <iostream>
+
+Deck main_deck;
 
 int main()
 {
+    const int DECK_SIZE = 5;
+    main_deck.addCard(new Card(Card::Card_Type::airlift));
+    main_deck.addCard(new Card(Card::Card_Type::blockade));
+    main_deck.addCard(new Card(Card::Card_Type::bomb));
+    main_deck.addCard(new Card(Card::Card_Type::diplomacy));
+    main_deck.addCard(new Card(Card::Card_Type::reinforcement));
+    // Ariel: A way to add a fixed amount of cards to a deck
+    //srand((unsigned) time(NULL));
+    //for(int i = 0; i < DECK_SIZE; ++i) {
+    //    int enum_index = rand() % Card::Card_Type::ENUM_SIZE;
+    //    deck.add(new Card(static_cast<Card::Card_Type>(enum_index)));
+    //}
+    Hand hand;
+    while (main_deck.getCards()->size() > 0) {
+        hand.addCard(main_deck.drawCard());
+    }
+    while(hand.getCards()->size() > 0) {
+        Card* card = hand.getCard(0);
+        card->play();
+    //    //std::cout << hand << std::endl;
+    //    hand.removeCard(0);
+    }
     return 0;
 }
