8bdb574e44267ad4eb4f99a2783ee4503bce2900
Shiraz <44529267+ShirazSevadjian@users.noreply.github.com>
2021-10-08 12:02:23 -0400

    Main commit for Orders section

--- src/Orders.cpp
+++ src/Orders.cpp
@@ -1,1 +1,350 @@
+#include <iostream>
 #include "Orders.h"
+
+using namespace std;
+
+//==================== Order Class ====================
+
+//Default Constructor
+Order::Order() {
+    this->orderName = "Order";
+}
+
+//Copy Constructor
+Order::Order(const Order& copy) {
+    this->orderName = copy.orderName;
+}
+
+//Parameterized Constructor
+Order::Order(const string &orderName) {
+    this->orderName = orderName;
+}
+
+//Getter for order name
+string Order::getName() {
+    return this->orderName;
+}
+
+//Setter for order name
+void Order::setName(const string& name) {
+    this->orderName = name;
+}
+
+//Destructor
+Order::~Order() { }
+
+//Input stream operator
+ostream& operator<<(ostream& os, Order& order){
+    return os << "Order type: " << order.getName() << endl;
+
+    //TODO check if executed, then print description
+}
+
+//Assignment Operator
+Order& Order::operator=(const Order& order) {
+    this->orderName = order.orderName;
+}
+
+
+
+
+
+
+
+
+
+//==================== OrderList Class ====================
+
+//Default Constructor
+OrdersList::OrdersList() { }
+
+//Copy Constructor
+OrdersList::OrdersList(const OrdersList& ordersList) {
+    for(int i = 0; i < ordersList.orderList.size(); i++){
+        this->orderList.push_back(ordersList.orderList[i]);
+    }
+}
+
+//Parameterized Constructor
+OrdersList::OrdersList(vector<Order *> orderList) {
+    this->orderList = orderList;
+}
+
+//Destructor
+OrdersList::~OrdersList() {
+    for(Order* o : orderList){
+        delete o;
+        o = nullptr;
+    }
+}
+
+//Removes an order from the specified index
+void OrdersList::removeOrder(int index) {
+    orderList.erase(orderList.begin() + index);
+    cout << "Deleted Order at index: " << index << endl;
+}
+
+//Move the order from index x (from) to index y (to)
+void OrdersList::move(int from, int to) {
+    //Save the element at index from as temp
+    Order *temporaryOrder = orderList[from];
+    //Remove the element at that the from index
+    orderList.erase(orderList.begin() + from);
+    //Insert the element that was at the from index (now saved in temp)
+    orderList.insert(orderList.begin() + to, temporaryOrder);
+    cout << "Moved order from index " << from << " to the index " << to << "." << endl;
+}
+
+//Add an order to the orders list
+void OrdersList::addOrder(Order *order) {
+    orderList.push_back(order);
+}
+
+
+
+
+//==================== Deploy Class ====================
+
+//Default Constructor
+Deploy::Deploy() {
+    this->orderName = "Deploy";
+}
+
+//Copy Constructor
+Deploy::Deploy(const Deploy& deploy) : Order(deploy) {
+    this->orderName = deploy.orderName;
+}
+
+//Destructor
+Deploy::~Deploy() {
+    cout << "Destroying order: deploy." << endl;
+}
+
+//Execute : First validates the order, and if valid executes its action
+bool Deploy::execute() {
+    //----- NO DESCRIPTION TO IMPLEMENT THIS FOR A1 -----
+    return false;
+}
+
+//Validate : checks if an order is valid
+bool Deploy::validate() {
+    //----- NO DESCRIPTION TO IMPLEMENT THIS FOR A1 -----
+    return false;
+}
+
+//Assignment operator
+Deploy &Deploy::operator=(const Deploy &order) {
+    Order::operator=(order);
+    this->orderName = order.orderName;
+    return *this;
+}
+
+ostream &operator<<(ostream &os, Deploy &deploy) {
+    return os << "Order name: " << deploy.getName();
+}
+
+
+
+
+
+
+// ==================== Advance Class ====================
+
+Advance::Advance() {
+    this->orderName = "Advance";
+}
+
+Advance::Advance(const Advance &advance) : Order(advance) {
+    this->orderName = advance.orderName;
+}
+
+Advance::~Advance() {
+    cout << "Destroying order: advance." << endl;
+}
+
+bool Advance::execute() {
+    //----- NO DESCRIPTION TO IMPLEMENT THIS FOR A1 -----
+    return false;
+}
+
+bool Advance::validate() {
+    //----- NO DESCRIPTION TO IMPLEMENT THIS FOR A1 -----
+    return false;
+}
+
+Advance &Advance::operator=(const Advance &advance) {
+    Order::operator=(advance);
+    this->orderName = advance.orderName;
+    return *this;
+}
+
+ostream &operator<<(ostream &os, Advance &advance) {
+    return os << "Order name: " << advance.getName();
+}
+
+
+
+
+
+
+
+// ==================== Bomb Class ====================
+
+
+Bomb::Bomb() {
+    this->orderName = "Bomb";
+}
+
+Bomb::Bomb(const Bomb &bomb) : Order(bomb) {
+    this->orderName = bomb.orderName;
+}
+
+Bomb::~Bomb() {
+    cout << "Destroying order: bomb." << endl;
+}
+
+bool Bomb::execute() {
+    //----- NO DESCRIPTION TO IMPLEMENT THIS FOR A1 -----
+    return false;
+}
+
+bool Bomb::validate() {
+    //----- NO DESCRIPTION TO IMPLEMENT THIS FOR A1 -----
+    return false;
+}
+
+Bomb &Bomb::operator=(const Bomb &bomb) {
+    Order::operator=(bomb);
+    this->orderName = bomb.orderName;
+    return *this;
+}
+
+ostream &operator<<(ostream &os, Bomb &bomb) {
+    return os << "Order name: " << bomb.getName();
+}
+
+
+
+
+// ==================== Blockade Class ====================
+
+
+Blockade::Blockade() {
+    this->orderName = "Blockade";
+}
+
+Blockade::Blockade(const Blockade &blockade) : Order(blockade){
+    this->orderName = blockade.orderName;
+}
+
+Blockade::~Blockade() {
+    cout << "Destroying order: blockade." << endl;
+}
+
+bool Blockade::execute() {
+    //----- NO DESCRIPTION TO IMPLEMENT THIS FOR A1 -----
+    return false;
+}
+
+bool Blockade::validate() {
+    //----- NO DESCRIPTION TO IMPLEMENT THIS FOR A1 -----
+    return false;
+}
+
+Blockade &Blockade::operator=(const Blockade &blockade) {
+    Order::operator=(blockade);
+    this->orderName = blockade.orderName;
+    return *this;
+}
+
+ostream &operator<<(ostream &os, Blockade &blockade) {
+    return os << "Order name: " << blockade.getName();
+}
+
+
+
+
+
+
+// ==================== Airlift Class ====================
+
+
+Airlift::Airlift() {
+    this->orderName = "Airlift";
+}
+
+Airlift::Airlift(const Airlift &airlift) : Order(airlift){
+    this->orderName = airlift.orderName;
+}
+
+Airlift::~Airlift() {
+    cout << "Destroying order: airlift." << endl;
+}
+
+bool Airlift::execute() {
+    //----- NO DESCRIPTION TO IMPLEMENT THIS FOR A1 -----
+    return false;
+}
+
+bool Airlift::validate() {
+    //----- NO DESCRIPTION TO IMPLEMENT THIS FOR A1 -----
+    return false;
+}
+
+Airlift &Airlift::operator=(const Airlift &airlift) {
+    Order::operator=(airlift);
+    this->orderName = airlift.orderName;
+    return *this;
+}
+
+ostream &operator<<(ostream &os, Airlift &airlift) {
+    return os << "Order name: " << airlift.getName();
+}
+
+
+
+
+
+
+
+
+
+
+// ==================== Negotiate Class ====================
+
+
+Negotiate::Negotiate() {
+    this->orderName = "Negotiate";
+}
+
+Negotiate::Negotiate(const Negotiate &negotiate) : Order(negotiate){
+    this->orderName = negotiate.orderName;
+}
+
+Negotiate::~Negotiate() {
+    cout << "Destroying order: airlift." << endl;
+}
+
+bool Negotiate::execute() {
+    //----- NO DESCRIPTION TO IMPLEMENT THIS FOR A1 -----
+    return false;
+}
+
+bool Negotiate::validate() {
+    //----- NO DESCRIPTION TO IMPLEMENT THIS FOR A1 -----
+    return false;
+}
+
+Negotiate &Negotiate::operator=(const Negotiate &negotiate) {
+    Order::operator=(negotiate);
+    this->orderName = negotiate.orderName;
+    return *this;
+}
+
+ostream &operator<<(ostream &os, Negotiate &negotiate) {
+    return os << "Order name: " << negotiate.getName();
+}
+
+
+
+
--- src/Orders.h
+++ src/Orders.h
@@ -1,9 +1,140 @@
-class Order
-{
+#pragma once
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+// ==================== Order Class ====================
+class Order {
+private:
+    string orderName;
+public:
+    Order();                                                            // Default Constructor
+    Order(const Order& copy);                                           // Copy Constructor
+    ~Order();                                                           // Destructor
+    explicit Order(const string& orderName);                            // Parameterized Constructor to define the type of Order
+    string getName();                                                   // Getter for the order name
+    void setName(const string& orderName);                              // Setter for the order name
+    Order & operator=(const Order& order);                              // Assignment operator overloading
+    friend ostream& operator <<(ostream& os, Order& order);             // Input stream operator
 };
 
-class OrdersList
-{
 
+// ==================== OrderList Class ====================
+class OrdersList {
+private:
+    vector<Order*> orderList;                                           // List (vector) of orders
+
+public:
+    OrdersList();                                                       // Default Constructor
+    OrdersList(const OrdersList& copy);                                 // Copy Constructor
+    explicit OrdersList(vector<Order*> orderList);
+    ~OrdersList();                                                      // Destructor
+    void removeOrder(int index);                                        // Removes the order from the list at specified index
+    void move(int from, int to);                                        // Moves an order from position x (from) to position y (to)
+    void addOrder(Order* order);
 };
+
+
+
+// ==================== Deploy Class ====================
+class Deploy : public Order {
+private:
+    string orderName;                                                   // Type of the order, in this case Deploy
+public:
+    Deploy();                                                           // Default Constructor
+    Deploy(const Deploy& deploy);                                       // Copy Constructor
+    ~Deploy();                                                          // Destructor
+    bool execute();                                                     // First validates the order, and if valid executes its action
+    bool validate();                                                    // Checks if the order is valid
+    Deploy& operator =(const Deploy& deploy);                           // Assignment operator overloading
+    friend ostream& operator<<(ostream& os, Deploy &deploy);            // Stream Insertion Operator
+};
+
+
+
+
+// ==================== Advance Class ====================
+class Advance : public Order {
+private:
+    string orderName;                                                   // Type of the order, in this case Deploy
+public:
+    Advance();                                                          // Default Constructor
+    Advance(const Advance& advance);                                    // Copy Constructor
+    ~Advance();                                                         // Destructor
+    bool execute();                                                     // First validates the order, and if valid executes its action
+    bool validate();                                                    // Checks if the order is valid
+    Advance& operator =(const Advance& advance);                        // Assignment operator overloading
+    friend ostream& operator<<(ostream& os, Advance &advance);          // Stream Insertion Operator
+};
+
+
+
+// ==================== Bomb Class ====================
+class Bomb : public Order {
+private:
+    string orderName;                                                   // Type of the order, in this case Deploy
+public:
+    Bomb();                                                             // Default Constructor
+    Bomb(const Bomb& bomb);                                             // Copy Constructor
+    ~Bomb();                                                            // Destructor
+    bool execute();                                                     // First validates the order, and if valid executes its action
+    bool validate();                                                    // Checks if the order is valid
+    Bomb& operator =(const Bomb& bomb);                                 // Assignment operator overloading
+    friend ostream& operator<<(ostream& os, Bomb &bomb);                // Stream Insertion Operator
+};
+
+
+
+
+// ==================== Blockade Class ====================
+class Blockade : public Order {
+private:
+    string orderName;                                                       // Type of the order, in this case Deploy
+public:
+    Blockade();                                                             // Default Constructor
+    Blockade(const Blockade& blockade);                                     // Copy Constructor
+    ~Blockade();                                                            // Destructor
+    bool execute();                                                         // First validates the order, and if valid executes its action
+    bool validate();                                                        // Checks if the order is valid
+    Blockade& operator =(const Blockade& blockade);                         // Assignment operator overloading
+    friend ostream& operator<<(ostream& os, Blockade &blockade);            // Stream Insertion Operator
+};
+
+
+
+
+// ==================== Airlift Class ====================
+class Airlift : public Order {
+private:
+    string orderName;                                                       // Type of the order, in this case Deploy
+public:
+    Airlift();                                                              // Default Constructor
+    Airlift(const Airlift& airlift);                                        // Copy Constructor
+    ~Airlift();                                                             // Destructor
+    bool execute();                                                         // First validates the order, and if valid executes its action
+    bool validate();                                                        // Checks if the order is valid
+    Airlift& operator =(const Airlift& airlift);                            // Assignment operator overloading
+    friend ostream& operator<<(ostream& os, Airlift &airlift);              // Stream Insertion Operator
+};
+
+
+
+
+
+// ==================== Negotiate Class ====================
+class Negotiate : public Order {
+private:
+    string orderName;                                                           // Type of the order, in this case Deploy
+public:
+    Negotiate();                                                                // Default Constructor
+    Negotiate(const Negotiate& negotiate);                                      // Copy Constructor
+    ~Negotiate();                                                               // Destructor
+    bool execute();                                                             // First validates the order, and if valid executes its action
+    bool validate();                                                            // Checks if the order is valid
+    Negotiate& operator =(const Negotiate& negotiate);                          // Assignment operator overloading
+    friend ostream& operator<<(ostream& os, Negotiate &negotiate);              // Stream Insertion Operator
+};
+
+
+
