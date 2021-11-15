1fd94edb45e3e9fc0b25d2fe425264d547d9abf1
volovikariel <16262535+volovikariel@users.noreply.github.com>
2021-11-14 20:47:41 -0500

    Implemented the stream insertion operator, copy constructor and assignment operators for all classes in LogObserver.

--- src/LoggingObserver.cpp
+++ src/LoggingObserver.cpp
@@ -11,6 +11,28 @@ Subject::Subject()
     observers = temp;
 }
 
+// Copy constructor for Subject
+Subject::Subject(const Subject& other):
+    observers(other.observers)
+{
+    
+}
+
+// Stream insertion operator
+ostream& operator << (ostream& out, const Subject& source)
+{
+    // Printing out the size of the observers list
+    out << "#Observers: " << source.observers.size();
+    return out;
+}
+
+// Assignment operator
+Subject& Subject::operator = (const Subject& other)
+{
+    // Shallow copy the Observer pointers
+    this->observers = other.observers;
+}
+
 // Destructor for the Subject
 Subject::~Subject()
 {
@@ -44,6 +66,27 @@ Observer::Observer()
 {
 
 }
+
+// Copy constructor for Observer (unused)
+Observer::Observer(const Observer& other)
+{
+
+}
+
+// Stream insertion operator
+ostream& operator << (ostream& out, const Observer& source)
+{
+    // The Observor doesn't really have any properties we can print out, so we just print out that it's an Observor
+    out << "Observer";
+    return out;
+}
+
+// Assignment operator (unused)
+Observer& Observer::operator = (const Observer& other)
+{
+
+}
+
 // Destructor for Observer
 Observer::~Observer()
 {
@@ -58,21 +101,70 @@ LogObserver::LogObserver()
 
 }
 
+// Copy constructor for Observer (unused)
+LogObserver::LogObserver(const LogObserver& other)
+{
+
+}
+
+// Stream insertion operator
+ostream& operator << (ostream& out, const LogObserver& source)
+{
+    // The LogObservor doesn't really have any properties we can print out, so we just print out that it's an LogObservor
+    out << "LogObserver";
+    return out;
+}
+
+// Assignment operator (unused)
+LogObserver& LogObserver::operator = (const LogObserver& other)
+{
+
+}
+
 // LogObserver destructor
 LogObserver::~LogObserver()
 {
     
 }
 
+
 // LogObserver update function inherited from the Observer class and overriden
-// Outputs to a "game.txt" file logs of what is going on
+// Outputs to a "gamelog.txt" file logs of what is going on
 void LogObserver::update(Subject& log)
 {
     ofstream ofs;
     // We're appending to the file, so we specify "app"
-    ofs.open("game.txt", std::ios_base::app);
+    ofs.open("gamelog.txt", std::ios_base::app);
     // The stringToLog function is only available for classes that inherit ILoggable, so we use dynamic_cast to make the compiler happy
     ofs << dynamic_cast<ILoggable*>(&log)->stringToLog();
     // Close the file stream
     ofs.close();
+}
+
+/* --- ILoggable --- */
+
+// ILoggable default constructor
+ILoggable::ILoggable()
+{
+
+}
+
+// Copy constructor for ILoggable (unused)
+ILoggable::ILoggable(const ILoggable& other)
+{
+
+}
+
+// Stream insertion operator
+ostream& operator << (ostream& out, const ILoggable& source)
+{
+    // ILoggable doesn't really have any properties we can print out, so we just print out that it's an ILoggable
+    out << "ILoggable";
+    return out;
+}
+
+// Assignment operator (unused)
+ILoggable& ILoggable::operator = (const ILoggable& other)
+{
+
 }
--- src/LoggingObserver.h
+++ src/LoggingObserver.h
@@ -9,8 +9,18 @@ class Subject;
 // ILoggable abstract class which requires the inheriting class to implement a stringToLog function
 class ILoggable {
     public:
-        // String to log function whose implementation specifies what is logged to the "game.txt" file
+        // Default constructor
+        ILoggable();
+        // Stream output operator of ILoggable
+        friend std::ostream& operator << (std::ostream& out, const ILoggable& source);
+        // String to log function whose implementation specifies what is logged to the "gamelog.txt" file
         virtual string stringToLog() = 0;
+    private:
+        // Observer copy constructor (unused)
+        ILoggable(const ILoggable& other);
+        // Assignment operator of ILoggable (unused)
+        ILoggable& operator = (const ILoggable& other);
+
 };
 
 class Observer {
@@ -19,8 +29,15 @@ class Observer {
         Observer();
         // Observer destructor
         virtual ~Observer();
+         // Stream output operator of Observer
+        friend std::ostream& operator << (std::ostream& out, const Observer& source);
         // Update method which signifies an update to the state was received and we will act on it (be it updating a view or in this case, writing to a file)
         virtual void update(Subject& subject) = 0;
+    private:
+        // Observer copy constructor (unused)
+        Observer(const Observer& other);
+        // Assignment operator of Observer (unused)
+        Observer& operator = (const Observer& other);
 };
 
 // Subject class which represents the Subject that will be observed
@@ -28,14 +45,21 @@ class Subject {
     public:
         // Subject constructor
         Subject();
+        // Subject copy constructor
+        Subject(const Subject& other);
         // Subject destructor
         ~Subject();
+        // Assignment operatorof Subject
+        Subject& operator = (const Subject& other);
+        // Stream output operator of Subject
+        friend std::ostream& operator << (std::ostream& out, const Subject& source);
         // Attach method which adds an Observer to "observers"
         virtual void attach(Observer& observer);
         // Detach method which removes an Observer from "observers"
         virtual void detach(Observer& observer);
         // Method which calls the "update" method for each Observer in "observers"
         virtual void notify();
+       
     private:
         list<Observer*> observers;
 };
@@ -47,6 +71,13 @@ class LogObserver : public Observer {
         LogObserver();
         // LogObserver destructor
         ~LogObserver();
+        // Stream output operator of LogObserver
+        friend std::ostream& operator << (std::ostream& out, const LogObserver& source);
         // Update method implementation inherited from Observer
         void update(Subject& subject);
+    private:
+        // LogObserver copy constructor (unused)
+        LogObserver(const LogObserver& other);
+        // Assignment operator of LogObserver (unused)
+        LogObserver& operator = (const LogObserver& other);
 };
