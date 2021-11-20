d976f948563e837fb0e468b60965fea27a259d41
Mark Beiline <markbeiline@gmail.com>
2021-11-20 18:43:25 -0500

    Setting up A3

--- src/PlayerStrategies.cpp
+++ src/PlayerStrategies.cpp
@@ -1,1 +1,134 @@
 #include "PlayerStrategies.h"
+#include "Orders.h"
+#include "Player.h"
+
+// PlayerStrategy
+
+PlayerStrategy::PlayerStrategy()
+{
+
+}
+
+PlayerStrategy::PlayerStrategy(Player& player):
+    player(&player)
+{
+
+}
+
+PlayerStrategy::PlayerStrategy(PlayerStrategy& other):
+    player(other.player)
+{
+
+}
+
+PlayerStrategy& PlayerStrategy::operator = (const PlayerStrategy& other)
+{
+    player = other.player;
+    return *this;
+}
+
+std::ostream& operator << (std::ostream& out, const PlayerStrategy& source)
+{
+    out << "PlayerStrategy: " << source.player;
+    return out;
+}
+
+
+// HumanPlayerStrategy
+
+HumanPlayerStrategy::HumanPlayerStrategy():
+    PlayerStrategy()
+{
+
+}
+
+HumanPlayerStrategy::HumanPlayerStrategy(Player& player):
+    PlayerStrategy(player)
+{
+
+}
+
+HumanPlayerStrategy::HumanPlayerStrategy(HumanPlayerStrategy& other):
+    PlayerStrategy(other)
+{
+
+}
+
+HumanPlayerStrategy& HumanPlayerStrategy::operator = (const HumanPlayerStrategy& other)
+{
+    PlayerStrategy::operator=(other);
+    return *this;
+}
+
+std::ostream& operator << (std::ostream& out, const HumanPlayerStrategy& source)
+{
+    out << "HumanPlayerStrategy: " << source.player;
+    return out;
+}
+
+Order* HumanPlayerStrategy::issueOrder(GameEngine& gameEngine)
+{
+    return nullptr;
+}
+
+vector<Territory*>& HumanPlayerStrategy::toAttack(GameEngine& gameEngine)
+{
+    vector<Territory*> tmp;
+    return tmp;
+}
+
+vector<Territory*>& HumanPlayerStrategy::toDefend(GameEngine& gameEngine)
+{
+    vector<Territory*> tmp;
+    return tmp;
+}
+
+
+// BenevolentPlayerStrategy
+
+BenevolentPlayerStrategy::BenevolentPlayerStrategy():
+    PlayerStrategy()
+{
+
+}
+
+BenevolentPlayerStrategy::BenevolentPlayerStrategy(Player& player):
+    PlayerStrategy(player)
+{
+
+}
+
+BenevolentPlayerStrategy::BenevolentPlayerStrategy(BenevolentPlayerStrategy& other):
+    PlayerStrategy(other)
+{
+
+}
+
+BenevolentPlayerStrategy& BenevolentPlayerStrategy::operator = (const BenevolentPlayerStrategy& other)
+{
+    PlayerStrategy::operator=(other);
+    return *this;
+}
+
+std::ostream& operator << (std::ostream& out, const BenevolentPlayerStrategy& source)
+{
+    out << "HumanPlayerStrategy: " << source.player;
+    return out;
+}
+
+Order* BenevolentPlayerStrategy::issueOrder(GameEngine& gameEngine)
+{
+    return nullptr;
+}
+
+vector<Territory*>& BenevolentPlayerStrategy::toAttack(GameEngine& gameEngine)
+{
+    vector<Territory*> tmp;
+    return tmp;
+}
+
+vector<Territory*>& BenevolentPlayerStrategy::toDefend(GameEngine& gameEngine)
+{
+    vector<Territory*> tmp;
+    return tmp;
+}
--- src/PlayerStrategies.h
+++ src/PlayerStrategies.h
@@ -1,1 +1,59 @@
 #pragma once
+#include "GameEngine.h"
+#include "Map.h"
+
+#include <vector>
+using std::vector;
+
+class Order;
+class Player;
+
+class PlayerStrategy
+{
+public:
+
+    PlayerStrategy();
+    PlayerStrategy(Player& player);
+    PlayerStrategy(PlayerStrategy& other);
+
+    PlayerStrategy& operator = (const PlayerStrategy& other);
+    friend std::ostream& operator << (std::ostream& out, const PlayerStrategy& source);
+
+    virtual Order* issueOrder(GameEngine& gameEngine) = 0;
+    virtual vector<Territory*>& toAttack(GameEngine& gameEngine) = 0;
+    virtual vector<Territory*>& toDefend(GameEngine& gameEngine) = 0;
+
+    Player* player;
+};
+
+class HumanPlayerStrategy : public PlayerStrategy
+{
+public:
+
+    HumanPlayerStrategy();
+    HumanPlayerStrategy(Player& player);
+    HumanPlayerStrategy(HumanPlayerStrategy& other);
+
+    HumanPlayerStrategy& operator = (const HumanPlayerStrategy& other);
+    friend std::ostream& operator << (std::ostream& out, const HumanPlayerStrategy& source);
+
+    Order* issueOrder(GameEngine& gameEngine) override;
+    vector<Territory*>& toAttack(GameEngine& gameEngine) override;
+    vector<Territory*>& toDefend(GameEngine& gameEngine) override;
+};
+
+class BenevolentPlayerStrategy : public PlayerStrategy
+{
+public:
+
+    BenevolentPlayerStrategy();
+    BenevolentPlayerStrategy(Player& player);
+    BenevolentPlayerStrategy(BenevolentPlayerStrategy& other);
+
+    BenevolentPlayerStrategy& operator = (const BenevolentPlayerStrategy& other);
+    friend std::ostream& operator << (std::ostream& out, const BenevolentPlayerStrategy& source);
+
+    Order* issueOrder(GameEngine& gameEngine) override;
+    vector<Territory*>& toAttack(GameEngine& gameEngine) override;
+    vector<Territory*>& toDefend(GameEngine& gameEngine) override;
+};
