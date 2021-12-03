bb4f0377297f7bac668e26a4f5f7476c008237c2
Shiraz <44529267+ShirazSevadjian@users.noreply.github.com>
2021-12-03 15:52:48 -0500

    Added some comments and a neutral player to the driver

--- src/PlayerStrategiesDriver.cpp
+++ src/PlayerStrategiesDriver.cpp
@@ -38,16 +38,19 @@ void test1(GameEngine& gameEngine)
     Player p1;
     p1.setName("NoStrategy");
 
+    //Aggressive Player
     Player p2;
     p2.setName("AggressiveStrategy");
     AggressivePlayerStrategy aggressiveStrategy(p2);
     p2.setPlayerStrategy(aggressiveStrategy);
 
+    //Cheating Player
     Player p3;
     p3.setName("CheatingStrategy");
     CheaterPlayerStrategy cheatingStrategy (p3);
     p3.setPlayerStrategy(cheatingStrategy);
 
+    //Benevolent Player
     Player p4;
     p4.setName("BStrategy");
     BenevolentPlayerStrategy BenevolentStrategy(p4);
@@ -55,12 +58,17 @@ void test1(GameEngine& gameEngine)
    // p4.toDefend(gameEngine);
     //BenevolentStrategy.toDefend(gameEngine);
 
+    //Neutral Player
+    Player p5;
+    p5.setName("NeutralStrategy");
+    NeutralPlayerStrategy neutralPlayerStrategy(p5);
+    p5.setPlayerStrategy(neutralPlayerStrategy);
+
     vector<Player*> players = { &p1, &p2, &p3, &p4 };
 
     gameEngine.start(mapFile, players);
 
     gameEngine.executeTurn();
-
 
 
 
